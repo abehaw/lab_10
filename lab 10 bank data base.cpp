47fb2cf7b28a225832612dcd1cb81d2c19ecb1d2
abehaw <ahawari@hawkmail.hfcc.edu>
2017-11-28 18:08:47 -0500

    fixing issues

---
+++ lab 10 bank data base.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct clientData
+{
+       int acountNumber;
+       char lastName[15];
+       char firstName[10];
+       double balance;
+};
+
+
+int main()
+{
+    //
+    
+    
+    
+    getchar();
+    return 0;
+}
